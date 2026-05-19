acd56aa05dc7df26659c237a801d089aacd7a042
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-05-19 23:51:07 +0100

    build: restructure to multi-module eco-core layout matching EcoBattlepass

---
+++ eco-core/core-plugin/build.gradle.kts
@@ -0,0 +1,32 @@
+group = "ru.oftendev"
+version = rootProject.version
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+tasks {
+    build {
+        dependsOn(publishToMavenLocal)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("shadow") {
+            from(components["java"])
+            artifactId = rootProject.name
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Auxilor"
+            url = uri("https://repo.auxilor.io/repository/maven-releases/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
