845d0c6bc5fb6ad9699eea726a4d91856dfda91d
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-05-20 23:25:14 +0100

    fix: extract RecipeSymmetry object; SymmetryTest references it directly (no libreforge dep needed)

---
+++ eco-core/core-plugin/src/main/kotlin/ru/oftendev/recipebook/custom/RecipeSymmetry.kt
@@ -0,0 +1,8 @@
+package ru.oftendev.recipebook.custom
+
+object RecipeSymmetry {
+    val ROT_90_CW  = intArrayOf(6, 3, 0, 7, 4, 1, 8, 5, 2)
+    val ROT_180    = intArrayOf(8, 7, 6, 5, 4, 3, 2, 1, 0)
+    val ROT_270_CW = intArrayOf(2, 5, 8, 1, 4, 7, 0, 3, 6)
+    val MIRROR_H   = intArrayOf(2, 1, 0, 5, 4, 3, 8, 7, 6)
+}
