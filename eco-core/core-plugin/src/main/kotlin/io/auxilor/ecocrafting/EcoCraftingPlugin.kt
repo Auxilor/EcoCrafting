cb13dc3cc4bf75b52ed4771c7ed42a9e29798a9a
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-07-07 09:52:23 +0100

    chore: refactor EcoCraftingPlugin structure

--- eco-core/core-plugin/src/main/kotlin/io/auxilor/ecocrafting/EcoCraftingPlugin.kt
+++ eco-core/core-plugin/src/main/kotlin/io/auxilor/ecocrafting/EcoCraftingPlugin.kt
@@ -1,20 +1,5 @@
 package io.auxilor.ecocrafting
 
-import io.auxilor.ecocrafting.category.CategoryLoader
-import io.auxilor.ecocrafting.commands.CommandEcoCrafting
-import io.auxilor.ecocrafting.custom.BlockOwnerTracker
-import io.auxilor.ecocrafting.custom.CustomRecipeListener
-import io.auxilor.ecocrafting.custom.CustomRecipeLoader
-import io.auxilor.ecocrafting.custom.RecipeUnlockStore
-import io.auxilor.ecocrafting.gui.RecipeCreatorChatListener
-import io.auxilor.ecocrafting.custom.libreforge.ConditionHasUnlockedRecipe
-import io.auxilor.ecocrafting.custom.libreforge.EffectLockRecipe
-import io.auxilor.ecocrafting.custom.libreforge.EffectUnlockRecipe
-import io.auxilor.ecocrafting.custom.libreforge.TriggerCustomCraft
-import io.auxilor.ecocrafting.custom.libreforge.TriggerGhostCraft
-import io.auxilor.ecocrafting.custom.libreforge.TriggerRecipeLocked
-import io.auxilor.ecocrafting.custom.libreforge.TriggerRecipeUnlocked
-import io.auxilor.ecocrafting.integration.ShopIntegration
 import com.willfp.eco.core.bstats.EcoMetricsChart
 import com.willfp.eco.core.command.impl.PluginCommand
 import com.willfp.libreforge.conditions.Conditions
@@ -22,6 +7,22 @@ import com.willfp.libreforge.effects.Effects
 import com.willfp.libreforge.loader.LibreforgePlugin
 import com.willfp.libreforge.loader.configs.ConfigCategory
 import com.willfp.libreforge.triggers.Triggers
+import io.auxilor.ecocrafting.category.CategoryLoader
+import io.auxilor.ecocrafting.commands.CommandEcoCrafting
+import io.auxilor.ecocrafting.custom.BlockOwnerTracker
+import io.auxilor.ecocrafting.custom.CustomRecipeListener
+import io.auxilor.ecocrafting.custom.CustomRecipeLoader
+import io.auxilor.ecocrafting.custom.RecipeUnlockStore
+import io.auxilor.ecocrafting.custom.libreforge.ConditionHasUnlockedRecipe
+import io.auxilor.ecocrafting.custom.libreforge.EffectLockRecipe
+import io.auxilor.ecocrafting.custom.libreforge.EffectUnlockRecipe
+import io.auxilor.ecocrafting.custom.libreforge.TriggerCustomCraft
+import io.auxilor.ecocrafting.custom.libreforge.TriggerGhostCraft
+import io.auxilor.ecocrafting.custom.libreforge.TriggerRecipeLocked
+import io.auxilor.ecocrafting.custom.libreforge.TriggerRecipeUnlocked
+import io.auxilor.ecocrafting.gui.RecipeCreatorChatListener
+import io.auxilor.ecocrafting.integration.ShopIntegration
+import org.bukkit.event.Listener
 
 internal lateinit var plugin: EcoCraftingPlugin
     private set
@@ -42,27 +43,36 @@ class EcoCraftingPlugin : LibreforgePlugin() {
         Effects.register(EffectUnlockRecipe)
         Effects.register(EffectLockRecipe)
         Conditions.register(ConditionHasUnlockedRecipe)
-
-        eventManager.registerListener(BlockOwnerTracker)
-        eventManager.registerListener(RecipeUnlockStore)
-        eventManager.registerListener(CustomRecipeListener())
-        eventManager.registerListener(RecipeCreatorChatListener)
     }
 
     override fun handleReload() {
         ShopIntegration.init(this)
     }
 
-    override fun loadConfigCategories(): List<ConfigCategory> {
-        return listOf(CategoryLoader, CustomRecipeLoader)
-    }
-
     override fun handleDisable() {
         RecipeUnlockStore.saveAll()
     }
 
-    override fun loadPluginCommands(): MutableList<PluginCommand> {
-        return mutableListOf(CommandEcoCrafting)
+    override fun loadConfigCategories(): List<ConfigCategory> {
+        return listOf(
+            CategoryLoader,
+            CustomRecipeLoader
+        )
+    }
+
+    override fun loadPluginCommands(): List<PluginCommand> {
+        return listOf(
+            CommandEcoCrafting
+        )
+    }
+
+    override fun loadListeners(): List<Listener> {
+        return listOf(
+            BlockOwnerTracker,
+            RecipeUnlockStore,
+            CustomRecipeListener(),
+            RecipeCreatorChatListener
+        )
     }
 
     override fun getCustomCharts() = listOf(
