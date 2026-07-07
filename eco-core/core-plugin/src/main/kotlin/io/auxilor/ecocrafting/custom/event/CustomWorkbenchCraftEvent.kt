5224d700cdd8ac539736923b955c266c0e29f8c8
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-07-07 10:29:37 +0100

    chore: align custom events with sibling conventions

--- eco-core/core-plugin/src/main/kotlin/io/auxilor/ecocrafting/custom/event/CustomWorkbenchCraftEvent.kt
+++ eco-core/core-plugin/src/main/kotlin/io/auxilor/ecocrafting/custom/event/CustomWorkbenchCraftEvent.kt
@@ -15,12 +15,24 @@ class CustomWorkbenchCraftEvent(
     val item: ItemStack,
     val stationType: RecipeDisplayType
 ) : PlayerEvent(player), Cancellable {
-    private var cancelled = false
-    override fun isCancelled() = cancelled
-    override fun setCancelled(cancel: Boolean) { cancelled = cancel }
-    override fun getHandlers() = HANDLER_LIST
+    private var _cancelled = false
+
+    override fun isCancelled() = _cancelled
+
+    override fun setCancelled(cancel: Boolean) {
+        _cancelled = cancel
+    }
+
+    override fun getHandlers(): HandlerList {
+        return handlerList
+    }
+
     companion object {
-        private val HANDLER_LIST = HandlerList()
-        @JvmStatic fun getHandlerList() = HANDLER_LIST
+        private val handlerList = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return handlerList
+        }
     }
 }
