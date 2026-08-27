6a16292afa86900c3231cc78bf16d018ccb594ad
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-08-27 11:00:03 +0100

    refactor: move eco-api-version to gradle.properties (#15)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
 group = "com.exanthiax"
 version = findProperty("version")!!
 val libreforgeVersion = findProperty("libreforge-version")
+val ecoApiVersion = findProperty("eco-api-version")
 val ecoVersion = findProperty("eco-version")
 
 base {
@@ -130,6 +131,7 @@ allprojects {
                 expand(
                     "version" to project.version,
                     "libreforgeVersion" to libreforgeVersion!!,
+                    "ecoApiVersion" to ecoApiVersion!!,
                     "pluginName" to rootProject.name
                 )
             }
